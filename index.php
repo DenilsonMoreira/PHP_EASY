f5eb59f37cb0492a3221734320d0989ae25b43a1
Denilsonk12 <43345513+Denilsonk12@users.noreply.github.com>
2020-06-22 09:12:20 -0300

    README.md
    Para criar um arquivo em php, basta iniciarmos com o cabeçário "<?php"  e o rodapé "?>".  Nesse projeto vou mostrar como comentar o código.

---
+++ index.php
@@ -0,0 +1,9 @@
+<?php
+#  O sinal a esquerda conhecido como hashtag é usado para comentar uma linha no php, # ou // tem o mesmo resultado
+/*
+Já a barra 
+asterisco lhe 
+permite comentar
+várias linhas
+*/
+?>
