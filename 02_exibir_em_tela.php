2a4e8941c51caaa3ab5ad64706e6e66c54a006ed
Denilson Moreira <43345513+Denilsonk12@users.noreply.github.com>
2021-04-17 08:40:53 -0300

    Create 02_exibir_em_tela.php

---
+++ 02_exibir_em_tela.php
@@ -0,0 +1,15 @@
+<?php
+# Na dúvida sempre leia o manual
+# https://www.php.net/manual/pt_BR/function.echo.php
+# https://www.php.net/manual/pt_BR/function.print-r
+# https://www.php.net/manual/pt_BR/function.print
+
+$texto "Hello my friend";
+echo $texto; 
+#Hello my friend
+print_r($texto);
+#Hello my friend
+
+
+
+?>
