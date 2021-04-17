5e4d4e69fdfc7392bdd69c0cb56c62cc6a8526bb
Denilson Moreira <43345513+Denilsonk12@users.noreply.github.com>
2021-04-17 11:49:47 -0300

    Create 02_variaveis.php

---
+++ 02_variaveis.php
@@ -0,0 +1,15 @@
+<?php
+$texo = "String";
+# Sempre use Aspas para criar strings
+$numero_inteiro = 10;
+# Números inteiros (int)
+$numero_float = 5.02;
+# Números Flutuantes (float)
+$vetor = array('chave_1' => 'valor_1', 'chave_2' => 'valor_2');
+$vetor_simples = array('valor_1', 'valor_2');
+$matriz = array(
+  array('valor_A1', 'valor_A2'),
+  array('valor_B1', 'valor_B2')
+);
+
+?>
